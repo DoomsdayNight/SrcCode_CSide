b70f33f9cbe58ef83f40cabc1860647fd9f95eaf
DoomsdayNight <129748642+DoomsdayNight@users.noreply.github.com>
2023-04-03 19:03:10 +0300

    Add files via upload

---
+++ Cs/Guild/NKMPacket_GUILD_UPDATE_MEMBER_GREETING_ACK.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Cs.Protocol;
+using NKM;
+using Protocol;
+
+namespace ClientPacket.Guild
+{
+	// Token: 0x02000EFD RID: 3837
+	[PacketId(ClientPacketId.kNKMPacket_GUILD_UPDATE_MEMBER_GREETING_ACK)]
+	public sealed class NKMPacket_GUILD_UPDATE_MEMBER_GREETING_ACK : ISerializable
+	{
+		// Token: 0x060098DA RID: 39130 RVA: 0x0032F173 File Offset: 0x0032D373
+		void ISerializable.Serialize(IPacketStream stream)
+		{
+			stream.PutOrGetEnum<NKM_ERROR_CODE>(ref this.errorCode);
+			stream.PutOrGet(ref this.greeting);
+		}
+
+		// Token: 0x04008B93 RID: 35731
+		public NKM_ERROR_CODE errorCode;
+
+		// Token: 0x04008B94 RID: 35732
+		public string greeting;
+	}
+}
