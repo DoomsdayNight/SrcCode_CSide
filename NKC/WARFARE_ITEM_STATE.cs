b37365d796ab039f4e7e8212bed1d8ea0e54bde7
DoomsdayNight <129748642+DoomsdayNight@users.noreply.github.com>
2023-04-03 15:14:14 +0300

    Add files via upload

---
+++ NKC/WARFARE_ITEM_STATE.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NKC
+{
+	// Token: 0x020007DF RID: 2015
+	public enum WARFARE_ITEM_STATE
+	{
+		// Token: 0x04003FCB RID: 16331
+		None,
+		// Token: 0x04003FCC RID: 16332
+		Question,
+		// Token: 0x04003FCD RID: 16333
+		Item,
+		// Token: 0x04003FCE RID: 16334
+		Recv
+	}
+}
